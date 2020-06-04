112476039e470c8660009f8433ae33f8a827ded7
Khalid Naz <khalidnaz17@gmail.com>
2020-06-04 15:43:53 -0700

    Add Makefile, migrations

---
+++ SolarCoffee.Data/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarCoffee.Data.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public DateTime CreateOn { get; set; }
+        public DateTime UpdateOn { get; set; }
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public CustomerAddress PrimaryAddress { get; set; }
+    }
+}
